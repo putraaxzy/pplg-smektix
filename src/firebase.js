34a0f7064c08138f7528449e1a03ad55bd11e1e6
muzakie624@gmail.com <muzakie624@gmail.com>
2023-11-14 23:55:20 +0700

    update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -9,15 +9,13 @@ import {getAuth, GoogleAuthProvider} from 'firebase/auth'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB3LV45-d2Do2U1qxzXZyzgKWZ6Es3vk_o",
-  authDomain: "pplg-smektix.firebaseapp.com",
-  projectId: "pplg-smektix",
-  storageBucket: "pplg-smektix.appspot.com",
-  messagingSenderId: "219991368812",
-  appId: "1:219991368812:web:6e418f22f55bf4e4a8e449",
-  measurementId: "G-GR00PBTXTL"
+  apiKey: "AIzaSyBmUIXJblRGF4HTCO3hOB8MykvLOq7whUE",
+  authDomain: "project-web-kelas.firebaseapp.com",
+  projectId: "project-web-kelas",
+  storageBucket: "project-web-kelas.appspot.com",
+  messagingSenderId: "202691312352",
+  appId: "1:202691312352:web:e82e7a494e705b2a12ee85"
 };
 
 // Initialize Firebase
