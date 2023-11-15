820973977322fda6046ef0eddd302f36c7aedfa1
muzakie624@gmail.com <muzakie624@gmail.com>
2023-11-15 13:51:40 +0700

    update StrukturKelas.jsx

--- src/Pages/StrukturKelas.jsx
+++ src/Pages/StrukturKelas.jsx
@@ -18,7 +18,7 @@ const StrukturKelas = () => {
 		<div className="z-1  relative h-auto lg:h-[83vh] lg:overflow-hidden">
 			{/* wali kelas */}
 			<div data-aos="fade-up" data-aos-duration="500" className="mt-14 md:mt-10">
-				<BorderStruktur Jabatan="Wali Kelas" Nama="Agus Sutrasno S.Pdi" Width="150px" />
+				<BorderStruktur Jabatan="Wali Kelas" Nama="Agus Sutrasno S.Pd" Width="150px" />
 			</div>
 			<div className="flex flex-col justify-center items-center">
 				<img src="LineVertikal.svg" alt="" data-aos="fade-up" data-aos-duration="550" />
