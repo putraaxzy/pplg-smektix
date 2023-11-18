c3df97f9cb91873e62773fbbb1da7fe9a567d356
muzakie624@gmail.com <muzakie624@gmail.com>
2023-11-18 11:02:44 +0700

    update Sabtu.jsx

--- src/components/Mapel/Sabtu.jsx
+++ src/components/Mapel/Sabtu.jsx
@@ -26,10 +26,10 @@ const Sabtu = () => {
 					<div className="w-[50%] text-base font-medium">ISTIRAHAT</div>
 					<div className="flex justify-center items-center text-sm">11:55-12:25</div>
 				</div>
-				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
-					<div className="w-[50%] text-base font-medium">PPLG</div>
-					<div className="flex justify-center items-center text-sm">12:25-13:45</div>
-				</div>
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
+				<div className="w-[50%] text-base  font-medium">PPLG</div>
+				<div className="flex justify-center items-center text-sm">12:25-13:45</div>
+								</div>
         </>
     )
 } 
