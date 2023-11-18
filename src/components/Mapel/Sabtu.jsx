533300aac512b7121f875680fe5d08b960e84424
muzakie624@gmail.com <muzakie624@gmail.com>
2023-11-18 11:08:42 +0700

    apa aja

--- src/components/Mapel/Sabtu.jsx
+++ src/components/Mapel/Sabtu.jsx
@@ -17,7 +17,6 @@ const Sabtu = () => {
 					<div className="w-[50%] text-base  font-medium">Bahasa Jawa</div>
 					<div className="flex justify-center items-center text-sm">09:55-11:15</div>
 				</div>
-
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
 					<div className="w-[50%] text-base  font-medium">PPLG</div>
 					<div className="flex justify-center items-center text-sm">11:15-11:55</div>
@@ -26,10 +25,10 @@ const Sabtu = () => {
 					<div className="w-[50%] text-base font-medium">ISTIRAHAT</div>
 					<div className="flex justify-center items-center text-sm">11:55-12:25</div>
 				</div>
-				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
+				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
 				<div className="w-[50%] text-base  font-medium">PPLG</div>
 				<div className="flex justify-center items-center text-sm">12:25-13:45</div>
-								</div>
+				</div>
         </>
     )
 } 
