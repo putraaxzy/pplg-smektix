71b46f5ac02a70b8f22e24c574030530c788a052
muzakie624@gmail.com <muzakie624@gmail.com>
2023-12-01 12:00:35 +0700

    update Jumat.jsx

--- src/components/Mapel/Jumat.jsx
+++ src/components/Mapel/Jumat.jsx
@@ -5,7 +5,7 @@ const Jumat = () => {
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">TKJ</div>
+				<div className="w-[50%] text-base  font-medium">Olahraga</div>
 				<div className="flex justify-center items-center text-sm">07.00-09.40</div>
 			</div>
 			<div
@@ -19,7 +19,7 @@ const Jumat = () => {
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="700">
-				<div className="w-[50%] text-base  font-medium">PPKn</div>
+				<div className="w-[50%] text-base  font-medium">PPLG</div>
 				<div className="flex justify-center items-center text-sm">09.40-11.40</div>
 			</div>
 		</>
