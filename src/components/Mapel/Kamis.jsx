d135115810f1e3bc198df14342fe069252fedfff
putraaxzy <wibowo.yunanto41@smk.belajar.id>
2024-02-01 18:31:17 +0700

    fix height logo & add jadwal kamis.
    Signed-off-by: putraaxzy <wibowo.yunanto41@smk.belajar.id>

--- src/components/Mapel/Kamis.jsx
+++ src/components/Mapel/Kamis.jsx
@@ -5,37 +5,37 @@ const Kamis = () => {
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">Mapel Kamis</div>
-				<div className="flex justify-center items-center text-sm">00:00-00:00</div>
+				<div className="w-[50%] text-base  font-medium">B. Indo</div>
+				<div className="flex justify-center items-center text-sm">07.15-08.35</div>
 			</div>
 			<div
 				className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">Mapel Kamis</div>
-				<div className="flex justify-center items-center text-sm">00:00-00:00</div>
+				<div className="w-[50%] text-base  font-medium">IPAS</div>
+				<div className="flex justify-center items-center text-sm">08.35-09.40</div>
 			</div>
 			<div
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="700">
-				<div className="w-[50%] text-base  font-medium">Mapel Kamis</div>
-				<div className="flex justify-center items-center text-sm">00:00-00:00</div>
+				<div className="w-[50%] text-base  font-medium">Matematika</div>
+				<div className="flex justify-center items-center text-sm">09.40-11.00</div>
 			</div>
 			<div
 				className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="800">
-				<div className="w-[50%] text-base  font-medium">Mapel Kamis</div>
-				<div className="flex justify-center items-center text-sm">00:00-00:00</div>
+				<div className="w-[50%] text-base  font-medium">B.Indo</div>
+				<div className="flex justify-center items-center text-sm">11.00-11.40</div>
 			</div>
 
 			<div
 				className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="1000">
-				<div className="w-[50%] text-base font-medium">Mapel Kamis</div>
-				<div className="flex justify-center items-center text-sm">00:00-00:00</div>
+				<div className="w-[50%] text-base font-medium">Informatika</div>
+				<div className="flex justify-center items-center text-sm">12.10-13.40</div>
 			</div>
 		</>
 	)
