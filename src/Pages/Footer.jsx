7a65a492fc63bf1fdbe54da4371c4cce1a7abd3e
ptra <144090584+putraaxzy@users.noreply.github.com>
2024-03-03 07:26:54 +0700

    add `github` links

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,77 +1,102 @@
-import Rating from "../components/Rating"
+import Rating from "../components/Rating";
 
 const Footer = () => {
-	return (
-		<footer class="" id="Footer">
-			<div class="mx-auto w-full max-w-screen-xl px-8 py-6 lg:py-8">
-				<div class="md:flex md:justify-between">
-					<div class="mb-6 md:mb-0">
-						<a href="#" class="flex items-center">
-							<img
-								src="/logo-pplg.png"
-								alt=""
-								className="h-[4.5rem] w-[4.5rem] right-2 relative rounded-full brightness-200"
-							/>
-							<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
-						</a>
-					</div>
-					<div class="grid grid-cols-2 sm:grid-cols-3">
-						<div className=" ">
-							<h2 class="mb-4 text-sm font-semibold uppercase text-white">CREATED BY</h2>
-							<ul class="text-gray-400 dark:text-gray-400 font-medium">
-								<li class="mb-2">
-								<a href="https://www.instagram.com/muzakie624/?hl=id" class="hover:underline">
-										MUZAKIE
-									</a>
-							</li>
-							<li class="mb-2">
-								<a href="https://www.instagram.com/onlyptraa" class="hover:underline">
-										PUTRA
-									</a>
-							</li>				
-							<li class="mb-2">
-								<a href="https://www.instagram.com/prim.m.t" class="hover:underline">
-										PRIMA
-									</a>
-							</li>		
-							</ul>						
-						</div>
-						<div className="">
-							<h2 class="mb-4 text-sm font-semibold uppercase text-white">Follow us</h2>
-							<ul class="text-gray-400 dark:text-gray-400 font-medium">
-								<li class="mb-2">
-									<a href="https://www.instagram.com/smektix_xpplg/?hl=id" class="hover:underline ">
-										Instagram
-									</a>
-								</li>
-								<li>
-									<a href="#" class="hover:underline">
-										Website
-									</a>
-								</li>
-							</ul>
-						</div>
-						<div className="hidden md:block">
-							<Rating />
-						</div>
-					</div>
-					
-					<div className="flex items-center justify-center mt-8  md:hidden">
-						{/* RATING */}
-						<Rating />
-					</div>
-				</div>
-				<hr class="my-2 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-				<div class="sm:flex sm:items-center sm:justify-between">
-					<div class="flex mt-4 justify-center sm:mt-0">
-						<p className="text-[0.7rem] text-white opacity-70">
-							© {new Date().getFullYear()} Kelas X PPLG | Di Kelola Oleh Siswa X PPLG
-						</p>
-					</div>
-				</div>
-			</div>
-		</footer>
-	)
-}
+  return (
+    <footer class="" id="Footer">
+      <div class="mx-auto w-full max-w-screen-xl px-8 py-6 lg:py-8">
+        <div class="md:flex md:justify-between">
+          <div class="mb-6 md:mb-0">
+            <a href="#" class="flex items-center">
+              <img
+                src="/logo-pplg.png"
+                alt=""
+                className="h-[4.5rem] w-[4.5rem] right-2 relative rounded-full brightness-200"
+              />
+              <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
+            </a>
+          </div>
+          <div class="grid grid-cols-2 sm:grid-cols-3">
+            <div className=" ">
+              <h2 class="mb-4 text-sm font-semibold uppercase text-white">
+                CREATED BY
+              </h2>
+              <ul class="text-gray-400 dark:text-gray-400 font-medium">
+                <li class="mb-2">
+                  <a
+                    href="https://www.instagram.com/muzakie624/?hl=id"
+                    class="hover:underline"
+                  >
+                    MUZAKIE
+                  </a>
+                </li>
+                <li class="mb-2">
+                  <a
+                    href="https://www.instagram.com/onlyptraa"
+                    class="hover:underline"
+                  >
+                    PUTRA
+                  </a>
+                </li>
+                <li class="mb-2">
+                  <a
+                    href="https://www.instagram.com/prim.m.t"
+                    class="hover:underline"
+                  >
+                    PRIMA
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="">
+              <h2 class="mb-4 text-sm font-semibold uppercase text-white">
+                Follow us
+              </h2>
+              <ul class="text-gray-400 dark:text-gray-400 font-medium">
+                <li class="mb-2">
+                  <a
+                    href="https://www.instagram.com/smektix_xpplg/?hl=id"
+                    class="hover:underline "
+                  >
+                    Instagram
+                  </a>
+                </li>
+                <li class="mb-2">
+                  <a
+                    href="https://github.com/ganesha-smektix"
+                    class="hover:underline "
+                  >
+                    Github
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="hover:underline">
+                    Website
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="hidden md:block">
+              <Rating />
+            </div>
+          </div>
 
-export default Footer
+          <div className="flex items-center justify-center mt-8  md:hidden">
+            {/* RATING */}
+            <Rating />
+          </div>
+        </div>
+        <hr class="my-2 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <div class="sm:flex sm:items-center sm:justify-between">
+          <div class="flex mt-4 justify-center sm:mt-0">
+            <p className="text-[0.7rem] text-white opacity-70">
+              © {new Date().getFullYear()} Kelas X PPLG | Di Kelola Oleh Siswa X
+              PPLG
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
